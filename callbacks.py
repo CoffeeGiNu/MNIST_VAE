8fe47e0d71d7f2c167605bef26c7bfee0225db5f
CoffeeGiNu <co4800@protonmail.com>
2022-02-18 20:27:23 +0900

    commit

--- callbacks.py
+++ callbacks.py
@@ -10,7 +10,7 @@ import numpy as np
 class EarlyStopping:
     """earlystoppingクラス"""
 
-    def __init__(self, patience=5, verbose=False, path='./models/checkpoint_model.pth'):
+    def __init__(self, patience=5, verbose=False, path='models/'):
         """引数：最小値の非更新数カウンタ、表示設定、モデル格納path"""
 
         self.patience = patience    #設定ストップカウンタ
@@ -47,5 +47,5 @@ class EarlyStopping:
         '''ベストスコア更新時に実行されるチェックポイント関数'''
         if self.verbose:  #表示を有効にした場合は、前回のベストスコアからどれだけ更新したか？を表示
             print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-        torch.save(model.to('cpu').state_dict(), self.path)  #ベストモデルを指定したpathに保存
+        torch.save(model.to('cpu').state_dict(), self.path+"checkpoint.pth")  #ベストモデルを指定したpathに保存
         self.val_loss_min = val_loss  #その時のlossを記録する
